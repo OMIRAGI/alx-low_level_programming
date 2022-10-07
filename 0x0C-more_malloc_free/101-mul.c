f92043cbf34111e20939dad286a14e05d01bd56e
OMIRAGI <omiragiruth@gmail.com>
2022-10-07 06:35:11 -0400

    finalized

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -28,7 +28,7 @@ int is_digit(char *s)
  * Return: the length of the string
  */
 int _strlen(char *s)
-{
+	{
 	int i = 0;
 
 	while (s[i] != '\0')
@@ -49,17 +49,17 @@ void errors(void)
 
 /**
  * main - multiplies two positive numbers
- * @argc: number of arguements
- * @argv: array of arguements
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-       char *s1, *s2;
-int len1, len2, len, i, carry, digit2, *result, a = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-s1 = argv[1], s2 = argv[2];
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,4 @@ void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
 
-
 #endif
